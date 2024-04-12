767a414b619f96a8d23ffffb3d3bf97f3c4d328d
jackson77-prog <jackson.njihia@student.moringaschool.com>
2024-04-12 13:21:55 +0300

    passed

--- lib/string_functions.py
+++ lib/string_functions.py
@@ -3,4 +3,7 @@ def return_string():
 
 def interpolate_string(s):
     return f'Hello, {s}!'
-    
+def test_interpolate_string():
+    '''Test for interpolate_string function.'''
+    result = interpolate_string('World')
+    assert result == 'Hello, World!'
